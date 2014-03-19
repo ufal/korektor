22aa1eb38e830162f60ef91ce2e2793acb2a678b
Milan Straka <fox@ucw.cz>
2014-03-19 09:09:52 +0100

    Add date and time to the logs.

--- rest_service/web/log.php
+++ rest_service/web/log.php
@@ -1,7 +1,7 @@
 <?php
 
 $line = strtr($_POST["original"], "\n\t", "  ") . "\t" . strtr($_POST["korektor"], "\n\t", "  ") . "\t" . strtr($_POST["corrected"], "\n\t", "  ") . "\n";
-if (strlen($line) > 3 && file_put_contents("/home/straka/korektor-logs/corrections.txt", $line, FILE_APPEND | LOCK_EX) !== false)
+if (strlen($line) > 3 && file_put_contents("/home/straka/korektor-logs/corrections.txt", date(DATE_RFC2822) . "\t" . $line, FILE_APPEND | LOCK_EX) !== false)
   echo '{"success": "true"}';
 
 ?>
