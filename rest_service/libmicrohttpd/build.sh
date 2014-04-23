f00d810fd7c107a2a7b9428503811d2b18795c16
Milan Straka <fox@ucw.cz>
2014-04-23 07:36:17 +0200

    Build both static and dynamic version of korektor_server.

--- rest_service/libmicrohttpd/build.sh
+++ rest_service/libmicrohttpd/build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-rm -rf libmicrohttpd.so* microhttpd.h
+rm -rf libmicrohttpd.a libmicrohttpd.so* microhttpd.h
 
 cd libmicrohttpd-*/
 ./configure --disable-curl --disable-https
 make -j4
-cp -a src/microhttpd/.libs/libmicrohttpd.so* src/include/microhttpd.h ..
+cp -a src/microhttpd/.libs/libmicrohttpd.a src/microhttpd/.libs/libmicrohttpd.so* src/include/microhttpd.h ..
 make distclean
