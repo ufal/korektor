fd7991bf4a3fed494f91524b160565a1ca5b029e
Milan Straka <fox@ucw.cz>
2014-10-31 15:59:24 +0100

    Upgrade to new (currently svn head) libmicrohttpd.

--- rest_service/libmicrohttpd/get.sh
+++ rest_service/libmicrohttpd/get.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.33.tar.gz
-tar xf libmicrohttpd-0.9.33.tar.gz
-rm libmicrohttpd-0.9.33.tar.gz
+#wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.33.tar.gz
+wget http://ufallab.ms.mff.cuni.cz/~straka/libmicrohttpd-0.9.38-milan.tar.gz
+tar xf libmicrohttpd-*.tar.gz
+rm libmicrohttpd-*.tar.gz
