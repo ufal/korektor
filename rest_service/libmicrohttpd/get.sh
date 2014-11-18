221278470ab230419b8f350c064d34051cbbe5df
Milan Straka <fox@ucw.cz>
2014-11-18 14:21:13 +0100

    Update libmicrohttpd to SVN-34406.

--- rest_service/libmicrohttpd/get.sh
+++ rest_service/libmicrohttpd/get.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.33.tar.gz
-wget http://ufallab.ms.mff.cuni.cz/~straka/libmicrohttpd-0.9.38-milan.tar.gz
+wget http://ufallab.ms.mff.cuni.cz/~straka/libmicrohttpd-0.9.38-svn34406.tar.gz
 tar xf libmicrohttpd-*.tar.gz
 rm libmicrohttpd-*.tar.gz
