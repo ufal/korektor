33aa8b0ba1e9a42eea9c536dbe770a01456a03a2
Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
2015-02-09 02:47:11 +0100

    Documentation for MyMappedDoubleArray

--- include/MyMappedDoubleArray.hpp
+++ include/MyMappedDoubleArray.hpp
@@ -3,6 +3,13 @@ Copyright (c) 2012, Charles University in Prague
 All rights reserved.
 */
 
+/// @file MyMappedDoubleArray.hpp
+/// @class MyMappedDoubleArray MyMappedDoubleArray.hpp "MyMappedDoubleArray.hpp"
+///
+/// @brief Class for holding mapped array
+///
+/// The class @ref MyMappedDoubleArray is used to store the mapped array of original values. The mapping is done via @ref ValueMapping.
+
 #ifndef MYMAPPEDDOUBLEARRAY_HPP_
 #define MYMAPPEDDOUBLEARRAY_HPP_
 
@@ -15,15 +22,22 @@ namespace ngramchecker {
 
 	class MyMappedDoubleArray {
 	private:
-		ValueMapping value_mapping;
-		MyPackedArray mpa;
+		ValueMapping value_mapping; ///< Mapping through @ref ValueMapping
+		MyPackedArray mpa; ///< Mapped values
 
 	public:
+		/// @brief Get the size of the mapped values (number of values)
+		///
+		/// @return Size (integer)
 		inline uint32_t GetSize() const
 		{
 			return mpa.GetSize();
 		}
 
+		/// @brief Get the mapped value at a given index
+		///
+		/// @param index Index
+		/// @return Mapped value (double)
 		inline double GetValueAt(uint32_t index) const
 		{
 			return value_mapping.GetDouble(mpa.GetValueAt(index));
--- src/MyMappedDoubleArray.cpp
+++ src/MyMappedDoubleArray.cpp
@@ -9,13 +9,23 @@ All rights reserved.
 
 namespace ngramchecker {
 
-
+		/// @brief Write mapped values to the output stream
+		///
+		/// @param ofs Output stream
 		void MyMappedDoubleArray::WriteToStream(ostream &ofs) const
 		{
 			value_mapping.writeToStream(ofs);
 			mpa.WriteToStream(ofs);
 		}
 
+		/// @brief Constructor initialization through vector of values
+		///
+		/// The constructor takes the vector of values and constructs the mapped values in 2 steps:
+		/// (i) values are mapped to larger set of values through @reg ValueMapping and (ii) mapped
+		/// values corresponding to the size of the original vector are taken from large mapped set.
+		///
+		/// @param values Vector of values
+		/// @param bits_per_value
 		MyMappedDoubleArray::MyMappedDoubleArray(vector<double> &values, uint32_t bits_per_value)
 		{
 			value_mapping = ValueMapping(values, bits_per_value);
@@ -30,6 +40,9 @@ namespace ngramchecker {
 			mpa = MyPackedArray(int_vals);
 		}
 
+		/// @brief Constructor initialization through input stream
+		///
+		/// @param ifs Input stream
 		MyMappedDoubleArray::MyMappedDoubleArray(istream &ifs):
 			value_mapping(ifs), mpa(ifs)
 		{}
