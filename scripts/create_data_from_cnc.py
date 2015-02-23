0007bc1016ec12a3e60c81b78a26e918a7da4e70
Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
2015-02-23 17:35:42 +0100

    Writing overhead is reduced

--- scripts/create_data_from_cnc.py
+++ scripts/create_data_from_cnc.py
@@ -43,6 +43,13 @@ def write_data(filename, outputDir):
         senOfLemmas = []
         senOfTags = []
         senOfFormTags = []
+
+        blockSize = 10000 # write 10k sentences per write operation
+        sentencesBlock = []
+        senOfLemmasBlock =[]
+        senOfTagsBlock= []
+        senOfFormTagsBlock = []
+
         senStarted = False
 
         # patterns in CNC file
@@ -50,15 +57,31 @@ def write_data(filename, outputDir):
         senStartPat = r'<s\s+id=\"\d+\">'
         senEndPat = r'<\/s>'
 
+        linesRead = 0
+        blockNumber = 1
+        print '--> Reading the input file ... ' + filename
         for line in fH:
             if re.search(senStartPat, line):
                 senStarted = True
                 continue
             if re.search(senEndPat, line) and senStarted:
-                fHandles[2].write(' '.join(sentence) + '\n')
-                fHandles[3].write(' '.join(senOfLemmas) + '\n')
-                fHandles[4].write(' '.join(senOfTags) + '\n')
-                fHandles[5].write(' '.join(senOfFormTags) + '\n')
+                linesRead += 1
+                sentencesBlock.append(' '.join(sentence))
+                senOfLemmasBlock.append(' '.join(senOfLemmas))
+                senOfTagsBlock.append(' '.join(senOfTags))
+                senOfFormTagsBlock.append(' '.join(senOfFormTags))
+                if linesRead == blockSize:
+                    fHandles[2].write('\n'.join(sentencesBlock) + '\n')
+                    fHandles[3].write('\n'.join(senOfLemmasBlock) + '\n')
+                    fHandles[4].write('\n'.join(senOfTagsBlock) + '\n')
+                    fHandles[5].write('\n'.join(senOfFormTagsBlock) + '\n')
+                    sentencesBlock = []
+                    senOfLemmasBlock =[]
+                    senOfTagsBlock= []
+                    senOfFormTagsBlock = []
+                    print '\t--> Read : ' + str(blockNumber * blockSize) + ' sentences'
+                    blockNumber += 1
+                    linesRead = 0
                 senStarted = False
                 sentence = []
                 senOfLemmas = []
@@ -74,6 +97,10 @@ def write_data(filename, outputDir):
                     tokens.append(senFound.group(1))
                     tokens.append(senFound.group(2))
                     tokens.append(senFound.group(3))
+
+                    # replace '|' with '@@@' in forms and lemmas
+                    tokens[0] = re.sub('\|', '@@@', tokens[0])
+                    tokens[1] = re.sub('\|', '@@@', tokens[1])
                     sentence.append(tokens[0])
                     senOfLemmas.append(tokens[1])
                     senOfTags.append(tokens[2])
@@ -86,14 +113,37 @@ def write_data(filename, outputDir):
                         counts[tokTupleStr] += 1
                     else:
                         counts[tokTupleStr] = 1
+        # unwritten stuff
+        if sentencesBlock:
+            fHandles[2].write('\n'.join(sentencesBlock) + '\n')
+            fHandles[3].write('\n'.join(senOfLemmasBlock) + '\n')
+            fHandles[4].write('\n'.join(senOfTagsBlock) + '\n')
+            fHandles[5].write('\n'.join(senOfFormTagsBlock) + '\n')
 
-    # write morphology lexicon and frequency counts
+    # write morphological lexicon and frequency counts
+    blockSize = 100000 # 100000 words per write operation
     fHandles[0].write('form|lemma|pos\n')
     fHandles[0].write('-----\n')
+    keyCountsList = []
+    keysList = counts.keys()
     for k in counts.keys():
-        fHandles[0].write(k + '\n')
-        fHandles[1].write(k + ' ' + str(counts[k]) + '\n')
+        keyCountsList.append(k + ' ' + str(counts[k]))
 
+    print '\n\t--> Writing morphological lexicon and frequency counts'
+    i = 0
+    while i * blockSize < len(keysList):
+        if (i+1) * blockSize > len(keysList):
+            fHandles[0].write('\n'.join(keysList[(i * blockSize):]) + '\n')
+            fHandles[1].write('\n'.join(keyCountsList[(i * blockSize):]) + '\n')
+        else:
+            fHandles[0].write('\n'.join(keysList[(i * blockSize): (i+1) * blockSize]) + '\n')
+            fHandles[1].write('\n'.join(keyCountsList[(i * blockSize): (i+1) * blockSize]) + '\n')
+        i += 1
+
+    print 'Done'
+
+    # close the input and output files
+    fH.close()
     close_file_handles(fHandles)
     return
 
@@ -112,11 +162,15 @@ def open_files_for_writing(filename, outputDir):
         lemmasH = codecs.open(lemmasFile, 'w', 'utf-8')
         tagsH = codecs.open(tagsFile, 'w', 'utf-8')
         formTagsH = codecs.open(formTagsFile, 'w', 'utf-8')
-        print 'Writing morphology lexicon to .... ' + morphLexiconFile
-        print 'Writing frequency counts to .... ' + freqFile
-        print 'Writing sentences to .... ' + sentencesFile
-        print 'Writing lemmas to .... ' + lemmasFile
-        print 'Writing tags to .... ' + tagsFile
+        print '*'*60
+        print 'Morphology lexicon will be written to .... ' + morphLexiconFile
+        print 'Frequency counts will be written to .... ' + freqFile
+        print 'Sentences  will be written to .... ' + sentencesFile
+        print 'Lemmas will be written to .... ' + lemmasFile
+        print 'Tags will be written to .... ' + tagsFile
+        print 'Forms/tags  will be written to .... ' + formTagsFile
+        print '*'*60
+        print '\n'
     except IOError as v:
         print v
     else:
