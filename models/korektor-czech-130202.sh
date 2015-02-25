7e5744bd59a92f95c9acedc19f207066ad844083
Milan Straka <fox@ucw.cz>
2015-02-25 12:43:26 +0100

    Add script for downloading korektor-czech-130202 model.

---
+++ models/korektor-czech-130202.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -O https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1460/korektor-czech-130202.zip
+unzip korektor-czech-130202.zip
+rm korektor-czech-130202.zip
