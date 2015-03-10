c03ed94456bb10970a392c6b8ad809a87d49cfdd
Milan Straka <fox@ucw.cz>
2015-03-10 08:45:14 +0100

    Move Parse class methods from header to source file.

---
+++ src/utils/parse.cpp
@@ -0,0 +1,42 @@
+// This file is part of korektor <http://github.com/ufal/korektor/>.
+//
+// Copyright 2015 by Institute of Formal and Applied Linguistics, Faculty
+// of Mathematics and Physics, Charles University in Prague, Czech Republic.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under 3-clause BSD licence.
+
+#pragma once
+
+#include <cstdlib>
+
+#include "parse.h"
+
+namespace ufal {
+namespace korektor {
+
+int Parse::Int(const string& str, const char* value_name) {
+  char* end;
+
+  errno = 0;
+  long result = strtol(str.c_str(), &end, 10);
+  if (*end || errno == ERANGE || result != int(result))
+    runtime_failure("Cannot parse int value " << value_name << ": '" << str << "'!");
+
+  return int(result);
+}
+
+double Parse::Double(const string& str, const char* value_name) {
+  char* end;
+
+  errno = 0;
+  double result = strtod(str.c_str(), &end);
+  if (*end || errno == ERANGE)
+    runtime_failure("Cannot parse double value " << value_name << ": '" << str << "'!");
+
+  return result;
+}
+
+} // namespace korektor
+} // namespace ufal
--- src/utils/parse.h
+++ src/utils/parse.h
@@ -9,8 +9,6 @@
 
 #pragma once
 
-#include <cstdlib>
-
 #include "common.h"
 
 namespace ufal {
@@ -18,28 +16,8 @@ namespace korektor {
 
 class Parse {
  public:
-  static int Int(const string& str, const char* value_name)
-  {
-    char* end;
-
-    errno = 0;
-    long result = strtol(str.c_str(), &end, 10);
-    if (*end || errno == ERANGE || result != int(result))
-      runtime_failure("Cannot parse " << value_name << " int value: '" << str << "'!");
-
-    return int(result);
-  }
-
-  static double Double(const string& str, const char* value_name) {
-    char* end;
-
-    errno = 0;
-    double result = strtod(str.c_str(), &end);
-    if (*end || errno == ERANGE)
-      runtime_failure("Cannot parse " << value_name << " double value: '" << str << "'!");
-
-    return result;
-  }
+  static int Int(const string& str, const char* value_name);
+  static double Double(const string& str, const char* value_name);
 };
 
 } // namespace korektor
