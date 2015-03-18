9f816c2633bb53f42fc1dce7c4f1ffda4065bb21
Milan Straka <fox@ucw.cz>
2015-03-18 20:47:36 +0100

    Update tests and benchmarks to use new korektor binary.
    Also now that new korektor is used, there is no need to update
    korektor-czech-130202 configuration files, so we reuse the
    models/korektor-czech-130202 model.

--- tests/korektor-czech-130202/benchmark.sh
+++ tests/korektor-czech-130202/benchmark.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-# Get the model and update the config files if needed.
-if [ \! -d korektor-czech-130202 ]; then
-  curl -O https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1460/korektor-czech-130202.zip
-  unzip korektor-czech-130202.zip
-  rm korektor-czech-130202.zip
-
-  sed 's/mode=tag_errors/mode=autocorrect/' -i korektor-czech-130202/*.conf
-fi
+# Get the model
+[ \! -d ../../models/korektor-czech-130202 ] && (cd ../../models && sh korektor-czech-130202.sh)
 
 # Compile korektor using release build
 make -C ../../src korektor MODE=release -j4 korektor
@@ -22,7 +16,7 @@ for conf_num in diacritics_h2mor:30 spellchecking_h2mor:4 spellchecking_h2mor_2e
   conf=${conf_num%:*}
   num=${conf_num##*:}
   echo Benchmarking $num-times the $conf model... >&2
-  time -p cat `for a in \`seq 1 $num\`; do echo povidky_z_jedne_kapsy.nodia; done` | ../../src/korektor korektor-czech-130202/$conf.conf >/dev/null
+  time -p cat `for a in \`seq 1 $num\`; do echo povidky_z_jedne_kapsy.nodia; done` | ../../src/korektor --input=untokenized_lines ../../models/korektor-czech-130202/$conf.conf >/dev/null
 done
 echo >&2
 echo >&2
--- tests/korektor-czech-130202/test.sh
+++ tests/korektor-czech-130202/test.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-# Get the model and update the config files if needed.
-if [ \! -d korektor-czech-130202 ]; then
-  curl -O https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1460/korektor-czech-130202.zip
-  unzip korektor-czech-130202.zip
-  rm korektor-czech-130202.zip
-
-  sed 's/mode=tag_errors/mode=autocorrect/' -i korektor-czech-130202/*.conf
-fi
+# Get the model
+[ \! -d ../../models/korektor-czech-130202 ] && (cd ../../models && sh korektor-czech-130202.sh)
 
 # Compile korektor.
 make -C ../../src korektor -j4 korektor
@@ -17,5 +11,5 @@ make -C ../../src korektor -j4 korektor
 # Test various configurations.
 for conf in diacritics_h2mor spellchecking_h2mor spellchecking_h2mor_2edits; do
   echo Testing $conf model... >&2
-  ../../src/korektor korektor-czech-130202/$conf.conf <povidky_z_jedne_kapsy.nodia | diff -u povidky_z_jedne_kapsy.$conf -
+  ../../src/korektor --input=untokenized_lines ../../models/korektor-czech-130202/$conf.conf <povidky_z_jedne_kapsy.nodia | diff -u povidky_z_jedne_kapsy.$conf -
 done
