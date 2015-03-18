7a0d18d5b1abe00b1e9910198cfa6ff9e12f4a1a
Milan Straka <fox@ucw.cz>
2015-03-18 20:33:30 +0100

    Remove original korektor and korektor_vectical by korektor_new.
    The korektor_new is now called korektor.

--- src/korektor.cpp
+++ src/korektor.cpp
@@ -7,158 +7,98 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted under 3-clause BSD licence.
 
-/// @file korektor.cpp
-/// @brief Main Korektor executable
-///
-/// usage: ./korektor configuration_file <input_text >output_text
-
-#include <cstring>
-#include <fstream>
-
 #include "common.h"
 #include "lexicon/lexicon.h"
 #include "spellchecker/configuration.h"
 #include "spellchecker/spellchecker.h"
-#include "token/tokenizer.h"
-#include "utils/io.h"
-#include "utils/utf.h"
+#include "token/input_format.h"
+#include "token/output_format.h"
+#include "utils/options.h"
+#include "utils/parse.h"
+#include "version/version.h"
 
 using namespace ufal::korektor;
 
-/// @brief Help message
-void print_help()
-{
-  cerr << "usage: ./korektor configuration_file <input_text >output_text" << endl;
-}
+int main(int argc, char* argv[]) {
+  iostream::sync_with_stdio(false);
 
-/// @brief Main entry point
-int main(int argc, char** argv)
-{
-  if (argc < 2)
-  {
-    print_help();
-    exit(1);
+  Options::Map options;
+  if (!Options::Parse({{"input",Options::Value{"untokenized", "untokenized_lines", "segmented", "vertical", "horizontal"}},
+                       {"output",Options::Value{"original", "xml", "vertical", "horizontal"}},
+                       {"corrections",Options::Value::any},
+                       {"context_free", Options::Value::none},
+                       {"version", Options::Value::none},
+                       {"help", Options::Value::none}}, argc, argv, options) ||
+      options.count("help") ||
+      (argc < 2 && !options.count("version")))
+    runtime_failure("Usage: " << argv[0] << " [options] model_configuration\n"
+                    "Options: --input=untokenized|untokenized_lines|segmented|vertical|horizontal\n"
+                    "         --output=original|xml|vertical|horizontal\n"
+                    "         --corrections=maximum_number_of_corrections\n"
+                    "         --context_free\n"
+                    "         --version\n"
+                    "         --help");
+  if (options.count("version"))
+    return cout << version::version_and_copyright() << endl, 0;
+
+  // Init options
+  unsigned max_corrections = 1;
+  if (options.count("corrections")) {
+    max_corrections = Parse::Int(options["corrections"], "maximum number of corrections");
+    if (max_corrections <= 0)
+      runtime_failure("The maximum number of corrections (" << max_corrections << ") must be at least 1!");
   }
 
+  bool context_free = options.count("context_free");
 
+  // Load spellchecker
   ConfigurationP configuration(new Configuration(argv[1]));
   Spellchecker spellchecker = Spellchecker(configuration.get());
 
-  //unsigned range_from, range_length;
-  //spellchecker.FindMisspelledWord("mrkev brambora pstrah", range_from, range_length);
+  // Init input format
+  string input_format_name = options.count("input") ? options["input"] : "untokenized";
+  auto input_format = InputFormat::NewInputFormat(input_format_name, configuration->lexicon);
+  if (!input_format)
+    runtime_failure("Cannot create input format '" << input_format_name << "'.");
 
-  //vector<TextCheckingResultP> result = spellchecker.GetCheckingResultsFirstSentence("Podlaha je pstrah. To je dobre\nFakt!", range_from, range_length);
+  // Init output format
+  string output_format_name = options.count("output") ? options["output"] : max_corrections == 1 ? "original" : "xml";
+  auto output_format = OutputFormat::NewOutputFormat(output_format_name);
+  if (!output_format)
+    runtime_failure("Cannot create output format '" << output_format_name << "'.");
 
-  //vector<string> suggestions = spellchecker.GetContextFreeSuggestions("pstryh");
+  // Check that output format supports multiple corrections if required
+  if (max_corrections > 1 && !output_format->CanHandleAlternatives())
+    runtime_failure("Output format '" << output_format_name << "' cannot handle multiple corrections!");
 
-  string s;
-  string out;
+  // Process data
+  string input_block, output_block;
+  vector<TokenP> tokens;
+  vector<SpellcheckerCorrection> corrections;
+  while (input_format->ReadBlock(cin, input_block)) {
+    input_format->SetBlock(input_block);
+    output_format->SetBlock(input_block);
+    output_block.clear();
 
-  if (argc >= 4 && strcmp(argv[2], "-gold") == 0)
-  {
-    ifstream gold_ifs(argv[3]);
-    if (gold_ifs.is_open() == false)
-    {
-      cerr << "Can't open gold data file!" << endl;
-      exit(1);
+    // For all sentences in the input block
+    while (input_format->NextSentence(tokens)) {
+      // Perform the spellchecking
+      if (!context_free) {
+        spellchecker.Spellcheck(tokens, corrections, max_corrections - 1);
+      } else {
+        corrections.resize(tokens.size());
+        for (unsigned i = 0; i < tokens.size(); i++)
+          spellchecker.SpellcheckToken(tokens[i], corrections[i], max_corrections - 1);
+      }
+
+      // Output the results
+      output_format->AppendSentence(output_block, tokens, corrections);
     }
 
-    unsigned fail = 0;
-    unsigned success = 0;
-
-    string gold_s;
-
-    unsigned counter = 0;
-
-    while (IO::ReadLine(cin, s))
-    {
-      counter++;
-
-      if (counter % 100 == 0) cerr << counter << endl;
-
-      if (gold_ifs.eof())
-      {
-        cerr << "Error: Gold data have fewer lines than input data!" << endl;
-        exit(1);
-      }
-
-      IO::ReadLine(gold_ifs, gold_s);
-
-      out = spellchecker.CheckText(s);
-
-      vector<vector<TokenP>> out_toks = configuration->tokenizer->Tokenize(UTF::UTF8To16(out));
-      vector<vector<TokenP>> gold_toks = configuration->tokenizer->Tokenize(UTF::UTF8To16(gold_s));
-
-      if (out_toks.size() != gold_toks.size())
-      {
-        cerr << "Error: input line splitted into different numbers of sentences in input data and gold data!" << endl;
-        cerr << "output: " << out << endl << endl;
-        cerr << "gold: " << gold_s << endl << endl;
-        exit(1);
-      }
-
-      for (unsigned i = 0; i < out_toks.size(); i++)
-      {
-        vector<TokenP> &out_t = out_toks[i];
-        vector<TokenP> &gold_t = gold_toks[i];
-
-        if (out_t.size() != gold_t.size())
-        {
-          cerr << "warning: different number of tokens per sentence on line '" << s << "' ! Sentence skipped!" << endl;
-          continue;
-        }
-
-        bool contains_error = false;
-        for (unsigned j = 0; j < out_t.size(); j++)
-        {
-          unsigned gold_wid = configuration->lexicon->GetWordID(gold_t[j]->str_u16);
-
-          //specific to current lexicon! (28 special words - punctuation and so - we don't care about these)
-          if (gold_wid >= 28)
-          {
-            if (gold_t[j]->str_utf8 == out_t[j]->str_utf8)
-              success++;
-            else
-            {
-              contains_error = true;
-              //failures.push_back("GOLD: " + gold_t[j]->str_utf8 + " - OUT: " + out_t[j]->str_utf8);
-              fail++;
-            }
-          }
-        }
-
-        if (contains_error)
-        {
-          cout << "GOLD:";
-          for (unsigned j = 0; j < gold_t.size(); j++)
-            cout << " " << gold_t[j]->str_utf8;
-          cout << "\nOUT:";
-          for (unsigned j = 0; j < out_t.size(); j++)
-            cout << " " << out_t[j]->str_utf8;
-          cout << endl << endl;
-        }
-      }
-    }
-
-    cout << "accuracy = " << success / (float)(success + fail) << endl;
-
-    return 0;
+    // Write the output block
+    output_format->FinishBlock(output_block);
+    cout << output_block;
   }
 
-  while (IO::ReadLine(cin, s))
-  {
-    if (configuration->mode_string == "tag_errors")
-    {
-      out = spellchecker.command_line_mode(s, 5);
-    }
-    else if (configuration->mode_string == "autocorrect")
-    {
-      out = spellchecker.CheckText(s);
-    }
-
-    cout << out << endl;
-  }
-
-  exit(0);
   return 0;
 }
