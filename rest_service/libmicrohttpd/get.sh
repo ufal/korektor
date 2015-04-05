0e077e5f33f0223b1f28922a67df0f669803502c
Milan Straka <fox@ucw.cz>
2015-04-05 12:29:42 +0200

    Remove previous version of REST server.
    The old pre-microrestd server is not needed now.

--- rest_service/libmicrohttpd/get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.33.tar.gz
-wget http://ufallab.ms.mff.cuni.cz/~straka/libmicrohttpd-0.9.38-svn34406.tar.gz
-tar xf libmicrohttpd-*.tar.gz
-rm libmicrohttpd-*.tar.gz
