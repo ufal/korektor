1e62a34b86f38384e23fd1b798d34594c11cd978
Milan Straka <fox@ucw.cz>
2015-06-29 19:30:44 +0200

    Update UniLib to 3.1.0 supporting Unicode 8.0.0.

--- src/unilib/version.h
+++ src/unilib/version.h
@@ -7,8 +7,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// UniLib version: 3.0.1
-// Unicode version: 7.0.0
+// UniLib version: 3.1.0
+// Unicode version: 8.0.0
 
 #pragma once
 
