4a5eb9bb3a4c97e69133ef92ef8bd27e3a7e99d0
Milan Straka <fox@ucw.cz>
2015-07-20 14:10:35 +0200

    Log the origin field if available.

--- web/lindat-service/log.php
+++ web/lindat-service/log.php
@@ -8,7 +8,7 @@ if (array_key_exists('original', $_POST) &&
     array_key_exists('korektor', $_POST) &&
     array_key_exists('corrected', $_POST)) {
   # Format log.
-  $line = trim(strtr($_POST["original"], "\n\t", "  ")) . "\t" . trim(strtr($_POST["korektor"], "\n\t", "  ")) . "\t" . trim(strtr($_POST["corrected"], "\n\t", "  ")) . "\n";
+  $line = trim(strtr($_POST["original"], "\n\t", "  ")) . "\t" . trim(strtr($_POST["korektor"], "\n\t", "  ")) . "\t" . trim(strtr($_POST["corrected"], "\n\t", "  ")) . (array_key_exists('origin', $_POST) ? "\t" . trim(strtr($_POST["origin"], "\n\t", "  ")) : "") . "\n";
 
   # Try appending it.
   if (strlen($line) > 3 && file_put_contents("/home/straka/korektor-logs/corrections.txt", date(DATE_RFC2822) . "\t" . $line, FILE_APPEND | LOCK_EX) !== false)
